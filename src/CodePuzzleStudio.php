c7ceb4d38cc3e81ce7ebb8ab989236c34b4937d3
ahsuoy <youshafarokey@gmail.com>
2018-05-02 16:45:46 +0600

    source folder added.

---
+++ src/CodePuzzleStudio.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Code Puzzle Studio PHP Framework
+ * 
+ */
+namespace CodePuzzleStudio;
+
+class CodePuzzleStudio {
+
+    /**
+     * 
+     */
+    public $databaseConfig;
+    /**
+     * 
+     */
+    public $dotEnv;
+
+    /**
+     * 
+     */
+    public function __construct($env, $database) {
+    
+    }
+}
